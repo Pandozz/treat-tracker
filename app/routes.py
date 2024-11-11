29c55309e9b597d8f8e695f5a35d3cbeeaa237f8
Pandozz <pandozgames@gmail.com>
2024-11-11 13:43:09 -0800

    Add basic html template for expenses.

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,38 @@
 from app import app
+from flask import render_template
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    mock_user = {'username': 'Pandozz'}
+
+    mock_expenses = [
+        {'date': '2024-11-11 14:30',
+         'amount': 6.50,
+         'category': 'Food',
+         'description': 'Icecream'},
+        {'date': '2024-11-12 08:45',
+         'amount': 5.00,
+         'category': 'Transportation',
+         'description': 'Bus fare'},
+        {'date': '2024-11-13 12:15',
+         'amount': 20.00,
+         'category': 'Entertainment',
+         'description': 'Movie ticket'},
+        {'date': '2024-11-14 18:00',
+         'amount': 15.99,
+         'category': 'Shopping',
+         'description': 'Gift for friend'},
+        {'date': '2024-11-15 10:00',
+         'amount': 20.00,
+         'category': 'Food',
+         'description': 'Breakfast'}
+    ]
+
+    return render_template(
+        'index.html',
+        title='Home',
+        user=mock_user,
+        expenses=mock_expenses
+    )
