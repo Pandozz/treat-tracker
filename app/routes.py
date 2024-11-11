a8629f7e21b7e21cba2eb5ce34caf57017c38007
Pandozz <pandozgames@gmail.com>
2024-11-11 14:37:51 -0800

    Add basic login form with error messaging.

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from app import app
-from flask import render_template
+from app.forms import LoginForm
+from flask import render_template, flash, redirect, url_for
 
 
 @app.route('/')
@@ -36,3 +37,15 @@ def index():
         user=mock_user,
         expenses=mock_expenses
     )
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data
+        ))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
