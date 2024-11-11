7730661f5a88dbe4e43117acc1e4b43307ac06d4
Pandozz <pandozgames@gmail.com>
2024-11-11 15:05:44 -0800

    add-database: Add basic User db model.

--- config.py
+++ config.py
@@ -2,5 +2,10 @@ import os
 import secrets
 
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or secrets.token_hex(32)
+    SQL_ALCHEMY_DATABASE_URL = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
